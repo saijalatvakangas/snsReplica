24fd9be0dd63c2758152af963bb7a16d5625ba5c
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-08-21 17:42:58 +0300

    added a scrolling view for posts

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::post('follow/{user}', 'FollowsController@store');
 
+Route::get('/', 'PostsController@index');
 Route::get('/post/create', 'PostsController@create');
 Route::post('/post', 'PostsController@store');
 Route::get('/post/{post}', 'PostsController@show');
