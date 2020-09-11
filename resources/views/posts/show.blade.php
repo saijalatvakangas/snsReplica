58e5f6a53c01e5fa3482d6c011cb303b72b1ef5d
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-11 13:52:06 +0300

    remove follow text from user's post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,7 +17,6 @@
                             <a href="/profile/{{ $post->user->id }}">
                                 <span class="text-dark">{{ $post->user->username }}</span>
                             </a>
-                            <a href="#" class="pl-3">Follow</a>
                         </div>
                     </div>
                 </div>
